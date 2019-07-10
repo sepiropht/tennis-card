00d3e0b2b426f579ca51a09e6509ab57a9f23191
sepiropht <sergembotta@mailoo.org>
2019-07-10 17:11:14 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+
+const Card = ({ player }) => (
+  <div>
+    <p>{JSON.stringify(player)}</p>
+  </div>
+);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [isLoaded, setLoad] = useState(false);
+  const [players, setPlayer] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch('/headtohead.json')
+      .then(result => result.json())
+      .then(json => {
+        setPlayer(json.players);
+        setLoad(true);
+      })
+      .catch(err => {
+        setError(err);
+        setLoad(true);
+      });
+  }, []);
+
+  const cards = players.map((player, index) => (
+    <Card key={index} player={player} />
+  ));
+
+  if (error) {
+    return <div> Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading cards..</div>;
+  } else {
+    return <div className="card">{cards}</div>;
+  }
 }
 
 export default App;
