86cdcc764670eb5a8c957bfd57913747af4b4a2a
sepiropht <sergembotta@mailoo.org>
2019-07-10 18:36:45 +0200

    improve ui

--- src/App.js
+++ src/App.js
@@ -6,10 +6,16 @@ const Div = styled.div`
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
   transition: 0.3s;
   width: 20%;
+  background-color: #d1b3ac;
   margin-top: 20px;
+  padding: 10px;
   &:hover {
     box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
   }
+  img {
+    border-radius: 5px 5px 0 0;
+  }
+  
 `;
 const Container = styled.div`
   padding: 2px 16px
@@ -28,14 +34,18 @@ const Card = ({ player }) => (
     </Container>
   </Div>
 );
-
+const Stat = styled.div`
+   display: flex;
+   
+   flex-direction: row;
+`
 const Stats = ({ data }) => (
-  <div className="stats">
-    <span>Numero°{data.rank} mondial</span>
-    <span> {data.points} pts</span>
-    <span> {data.weight} kg</span>
-    <span> {data.age} ans</span>
-  </div>
+  <Stat>
+    <span> <strong>Ranking</strong>: {data.rank} </span>
+    <span> <strong>Point ATP</strong> {data.points} pts</span>
+    <span> <strong>Poids</strong> {data.weight} kg</span>
+    <span> <strong>Age</strong> {data.age} ans</span>
+  </Stat>
 );
 function App() {
   const [isLoaded, setLoad] = useState(false);
